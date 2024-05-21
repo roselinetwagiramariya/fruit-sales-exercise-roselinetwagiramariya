9218d71b4b88cbfcbea7d60378aa1b9a8fba7fe4
Roseline Twagiramariya <157075654+roselinetwagiramariya@users.noreply.github.com>
2024-05-21 15:59:38 -0400

    Update fruitsales.py

--- fruitsales.py
+++ fruitsales.py
@@ -2,8 +2,7 @@ import pandas as pd
 
 fruit_sales = pd.DataFrame({
     'Apples': [35, 41],  
-    'Bananas': [21, 34]  
-})
+    'Bananas': [21, 34]  })
 
 
 fruit_sales.index = [‘2017 Sales’, ‘2018 Sales’]
