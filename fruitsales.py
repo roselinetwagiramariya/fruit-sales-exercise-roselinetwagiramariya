fc8824bab34c02c9b39ba65b04df7f2cca2fbec6
Roseline Twagiramariya <157075654+roselinetwagiramariya@users.noreply.github.com>
2024-05-21 16:07:06 -0400

    Update fruitsales.py

--- fruitsales.py
+++ fruitsales.py
@@ -4,8 +4,7 @@ fruit_sales = pd.DataFrame({
     'Apples': [35, 41],  
     'Bananas': [21, 34]  })
 
-
-fruit_sales.index = [‘2017 Sales’, ‘2018 Sales’]
+fruit_sales.index = ['2017 Sales', '2018 Sales'] 
 
 fruit_sales.to_csv('fruit_sales.csv')
 
