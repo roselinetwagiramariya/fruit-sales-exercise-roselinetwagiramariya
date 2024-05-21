9c223850e1f38623d0284b23cbdd3c9676d1c17a
Roseline Twagiramariya <157075654+roselinetwagiramariya@users.noreply.github.com>
2024-05-21 15:58:13 -0400

    Update fruitsales.py

--- fruitsales.py
+++ fruitsales.py
@@ -1,1 +1,14 @@
+import pandas as pd  
+
+fruit_sales = pd.DataFrame({
+    'Apples': [35, 41],  # Sales figures for apples
+    'Bananas': [21, 34]  # Sales figures for bananas
+})
+
+# Add an index to the DataFrame to label the rows
+fruit_sales.index = [‘2017 Sales’, ‘2018 Sales’]
+
+fruit_sales.to_csv('fruit_sales.csv')
+
+print(fruit_sales)
 
